9f4f31a1ac863f7946f40f9bfaf087cf23ed865c
rlaalsghks8 <127469367+rlaalsghks8@users.noreply.github.com>
2023-07-01 16:32:45 +0900

    mvc2-thymeleaf 기본기능 수업정리

---
+++ thymeleaf-basic/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -0,0 +1,29 @@
+package hello.thymeleaf.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+
+    @GetMapping
+    public String template(){
+        return "template/fragment/fragmentMain";
+    }
+
+    @GetMapping("layout")
+    public String layout(){
+        return "template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtends() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
+
+
+
+}
